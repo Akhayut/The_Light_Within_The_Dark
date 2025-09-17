3cadaf3c6dda37de78cd4de719ed75096e672316
Akhayut <akhayut12@gmail.com>
2025-09-17 13:46:03 -0600

    pushed

--- the-light-within-the-dark/scripts/scripts/characters/bangle_berry.gd
+++ the-light-within-the-dark/scripts/scripts/characters/bangle_berry.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+#var defineing
 var mousePos
 var moving = false
-@export var speed = 6700
+@export var speed = 167
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	#defineing distance
 	mousePos = get_global_mouse_position()
 	var distanceToMouse = sqrt(((global_position.x - mousePos.x)**2.0) +((global_position.y - mousePos.y)**2.0))
@@ -16,23 +17,14 @@ func _process(delta: float) -> void:
 	elif  mousePos.x - global_position.x < 2:
 		$sprite.flip_h = false
 		
-	if abs(velocity.x) < 0.001 or abs(velocity.y) < 0.001:
-		moving = false
-	else:
-		moving = true
-		
-	print(velocity)
-		
-	if moving == false:
-		$sprite.animation = "idle"
-	else:
+	if abs(velocity.x) > 0.5 or abs(velocity.y) > 0.5:
 		$sprite.animation = "walk"
-	
-	
+	else:
+		$sprite.animation = "idle"
 	
 	#movement
 	if distanceToMouse > 2:
-		velocity = (( mousePos - global_position) / distanceToMouse) * (speed * delta)
+		velocity = (( mousePos - global_position) / distanceToMouse) * speed
 	else:
 		velocity = Vector2(0,0)
 	move_and_slide()
--- the-light-within-the-dark/scripts/scripts/maps/lightScript.gd
+++ the-light-within-the-dark/scripts/scripts/maps/lightScript.gd
@@ -1,15 +1,18 @@
 extends ColorRect
 
 func _ready() -> void:
+	#unhide rect
 	visible = true
 
 func _process(_delta: float) -> void:
+	#define view port and screen size
 	var vp = get_viewport()
 	var screen_size: Vector2 = Vector2(vp.get_visible_rect().size)
 
+	#gets mouse pos and translates it to UV cords
 	var mouse_screen: Vector2 = Vector2(vp.get_mouse_position())
 	var mouse_uv: Vector2 = mouse_screen / screen_size
 
+	#sets shader parameters
 	material.set_shader_parameter("mousePos", mouse_uv)
-
 	material.set_shader_parameter("aspect", screen_size.x / screen_size.y)
