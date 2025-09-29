a5075bfaf16f5232585c844b9386bd3ed5dbc800
Akhayut <akhayut12@gmail.com>
2025-09-29 09:51:13 -0600

    comments

--- the-light-within-the-dark/scripts/scripts/characters/bangle_berry.gd
+++ the-light-within-the-dark/scripts/scripts/characters/bangle_berry.gd
@@ -10,12 +10,12 @@ func _process(_delta: float) -> void:
 	mousePos = get_global_mouse_position()
 	var distanceToMouse = sqrt(((global_position.x - mousePos.x)**2.0) +((global_position.y - mousePos.y)**2.0))
 	
-	#flipping sprite/animation
+	#flipping sprite
 	if  mousePos.x - global_position.x > 2:
 		$sprite.flip_h = true
 	elif  mousePos.x - global_position.x < 2:
 		$sprite.flip_h = false
-		
+	#animation
 	if abs(velocity.x) > 0.5 or abs(velocity.y) > 0.5:
 		$sprite.animation = "walk"
 	else:
